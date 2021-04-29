9ff0263d2c9140ed5101578d31b7fcb75703d50c
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-04-29 10:48:14 +0200

    Phonebook

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,31 @@
+package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Any parameter is null..");
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+            writeLines(contacts, writer);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+    }
+
+    private void writeLines(Map<String, String> contacts, BufferedWriter writer) {
+        try {
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+    }
+}
